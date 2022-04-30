bff6dd3f1218ae2daa8b8ae3e1f177759ea178c4
渲宝宝mua <103923327+16and37@users.noreply.github.com>
2022-04-30 17:16:40 +0800

    Update lovetime.js

--- js/lovetime.js
+++ js/lovetime.js
@@ -1,28 +1,21 @@
 function lovetime() {
     window.setTimeout("lovetime()", 1000);
-    var seconds = 1000
-    var minutes = seconds * 60
-    var hours = minutes * 60
-    var days = hours * 24
-    var years = days * 365
-    var today = new Date()
-    var todayYear = today.getFullYear()
-    var todayMonth = today.getMonth()
-    var todayDate = today.getDate()
-    var todayHour = today.getHours()
-    var todayMinute = today.getMinutes()
-    var todaySecond = today.getSeconds()
-    // 修改这里时间即可 我的是2021.05.04
-    var t1 = Date.UTC(2022, 03, 15, 00, 00, 00)
-    var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
-    var diff = t2 - t1
-    var diffYears = Math.floor(diff / years)
-    var diffDays = Math.floor((diff / days) - diffYears * 365)
-    var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours)
-    var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
-    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
-        minutes) / seconds)
-    document.getElementById("lovetime").innerHTML = "我们已经在一起 " + diffYears + "年" + diffDays + "天" +
-        diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
+    function timingTime(){
+      let start = '2022-3-15 10:00:00'
+      let startTime = new Date(start).getTime()
+      let currentTime = new Date().getTime()
+      let difference = currentTime - startTime
+      let m =  Math.floor(difference / (1000))
+      let mm = m % 60  // 秒
+      let f = Math.floor(m / 60)
+      let ff = f % 60 // 分钟
+      let s = Math.floor(f/ 60) // 小时
+      let ss = s % 24
+      let day = Math.floor(s  / 24 ) // 天数
+      return "我们已经在一起"day + "天" + ss + "时" + ff + "分" + mm +'秒啦'
+    }
+    setInterval(()=>{
+      document.getElementById('box1').innerHTML = timingTime()
+    },1000)
 }
 lovetime()
